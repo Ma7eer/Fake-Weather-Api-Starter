64fd91a6129c9d5f62743b8f325c58ae29570487
Maher Alkendi <maher.alkendi2@gmail.com>
2018-11-17 07:04:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // import express
+const app = express(); // initialize express
 
+// Listen on port 3000
 app.listen(3000, function() {
   console.log('listening on port 3000...');
 })
